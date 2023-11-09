2fd902b9b362f5ba68a63b9447877172be1ba8bf
ayaaalli <ayaali95248@gmail.com>
2023-11-09 23:51:24 +0200

    network & semaphore classes

--- src/Network.java
+++ src/Network.java
@@ -1,2 +1,26 @@
+import java.util.*;
+
+import static java.lang.Thread.sleep;
+
 public class Network {
+    public static void main(String[] args) throws InterruptedException {
+        int NumberOfConnections;
+        int NumberOfDevices;
+        List<Device> devices = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the number of WI-FI Connections?");
+        NumberOfConnections = input.nextInt();
+        Router router = new Router(NumberOfConnections);
+        System.out.println("What is the number of devices Clients want to connect?");
+        NumberOfDevices = input.nextInt();
+        for (int i = 0; i < NumberOfDevices; i++) {
+            Device D = new Device(input.next(), input.next(),router);
+            devices.add(D);
+        }
+        for (int i = 0; i < NumberOfDevices; i++) {
+            sleep(100);
+            devices.get(i).start();
+        }
+
+    }
 }
--- src/Semaphore.java
+++ src/Semaphore.java
@@ -1,1 +1,28 @@
 
+class Semaphore{
+    protected int value;
+    protected Semaphore(int initial) {
+        value = initial ;
+    }
+    public synchronized void wait(Device device) throws InterruptedException {
+        value-- ;
+        if (value < 0){
+            try {
+                System.out.println(device.Name + " (" + device.Type + ")" + " arrived and waiting");
+                wait() ;
+            }
+            catch( InterruptedException e ) {
+                System.out.println("Device has been interrupted while waiting.");
+            }
+        }else{
+            System.out.println( device.Name +" (" + device.Type + ")" +" arrived");
+        }
+        device.router.connect(device);
+    }
+    public synchronized void signal() {
+        value++ ;
+        if (value <= 0) {
+            notify() ;
+        }
+    }
+}
