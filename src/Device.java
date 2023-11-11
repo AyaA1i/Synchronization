3404b22bb32b7c86746e30c3e179ac1bbfbe9e77
ayaaalli <ayaali95248@gmail.com>
2023-11-11 20:58:12 +0200

    last

--- src/Device.java
+++ src/Device.java
@@ -10,36 +10,42 @@ public class Device extends Thread {
         this.router = router;
         this.semaphore = semaphore;
     }
-
-    @Override
-    public void run() {
+    private int connectDevice(){
         int connection = router.Connect();
         if (connection == -1) {
             System.out.println("(" + this.Name + ")(" + this.Type + ") arrived and waiting");
-            try {
-                semaphore.Wait();
-            } catch (InterruptedException e) {
-                System.out.println("Device is interrupted.");
-            }
-            connection = router.Connect();
         } else {
             System.out.println("(" + this.Name + ") " + "(" + this.Type + ") " + "arrived");
         }
-
+        try {
+            semaphore.Wait();
+        } catch (InterruptedException e) {
+            System.out.println("Device is interrupted.");
+        }
+        if (connection == -1) connection = router.Connect();
         System.out.println("Connection " + (connection + 1) + ": " + this.Name + " Occupied");
         System.out.println("Connection " + (connection + 1) + ": " + this.Name + " login");
+        return connection;
+    }
+    private void doActivity(int connection){
         System.out.println("Connection " + (connection + 1) + ": " + this.Name + " performs online activity");
-        // Perform online activity
-        // Simulate online activity with random waiting time
-
         try {
             Thread.sleep(1000); // Simulating online activity
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+    }
+    private void disconnectDevice(int connection){
         System.out.println("Connection " + (connection + 1) + ": " + Name + " Logged out");
         router.Disconnect(connection);
         semaphore.signal();
     }
+    @Override
+    public void run() {
+        int connection = connectDevice();
+        doActivity(connection);
+        disconnectDevice(connection);
+    }
+
 }
